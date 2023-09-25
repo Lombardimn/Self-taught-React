661efda8c61aa68cbbfe2923afca90b8939beff8
Matias Lombardi <matiiaslombardi@gmail.com>
2023-09-25 03:58:26 -0300

    V.1.1.0 - Project 01

--- aprendiendo-react-midudev/01-twitter-follow-card/src/App.jsx
+++ aprendiendo-react-midudev/01-twitter-follow-card/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TwitterFollowCard } from './components/TwitterFollowCard'
+import './css/app.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+    {
+        userName: 'lombardimn',
+        name: 'Matias Lombardi',
+        isFollowing: true
+    },
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: false
+    },
+    {
+        userName: 'manzdev',
+        name: 'José Román Hernández Martín',
+        isFollowing: true
+    },
+    {
+        userName: 'chrisvdev',
+        name: 'Christian Villegas',
+        isFollowing: false
+    }
+]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    return (
+        <>
+        <h1 className='App-title'>Model Twitter Follow Card</h1>
+        <div className='App-border'>
+            <section className='App'>
+                {
+                    users.map(({userName, name, isFollowing})=>(
+                        <TwitterFollowCard 
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    ))
+                }
+            </section>
+        </div>
+        </>
+    )
 }
-
-export default App
