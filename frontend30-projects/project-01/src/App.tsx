fff58767e5c7574f90eb699c770113db2b866813
Matias Lombardi <matiiaslombardi@gmail.com>
2024-10-09 01:05:36 -0300

    fix: frontend30/project-01

--- frontend30-projects/project-01/src/App.tsx
+++ frontend30-projects/project-01/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h1>QR code component</h1>
 
-      <Card image="/public/image-qr.png" imgAlt="QR code">
+      <Card image="/image-qr.png" imgAlt="QR code">
         <h2 className="card-title">Improve your front-end skills by building projects</h2>
         <p className="card-description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </Card>
