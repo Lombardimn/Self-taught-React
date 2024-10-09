3d13af98cb1896eebb0475295618cb7cc24e48f9
Lombardimn <matiiaslombardi@gmail.com>
2024-10-09 17:44:23 -0300

    refactor: [frontend30/project-01]-Accessibility

--- frontend30-projects/project-01/src/App.tsx
+++ frontend30-projects/project-01/src/App.tsx
@@ -3,14 +3,14 @@ import { Card } from "./components/Card"
 function App() {
 
   return (
-    <>
+    <main role="main">
       <h1>QR code component</h1>
 
       <Card image="/image-qr.png" imgAlt="QR code">
         <h2 className="card-title">Improve your front-end skills by building projects</h2>
         <p className="card-description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </Card>
-    </>
+    </main>
   )
 }
 
